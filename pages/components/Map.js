5cb5c5db70c8d92af9b6b0ff1f99f32f993e1d9b
jeswanth <48980002+jeswanthv@users.noreply.github.com>
2025-05-09 11:56:13 -0700

    feat: Update Map component to support dynamic marker colors and route IDs

--- pages/components/Map.js
+++ pages/components/Map.js
@@ -27,15 +27,30 @@ export const Map = (props) => {
       [-122.168861, 37.42823],
     ];
 
+    const colors = ["#EC5F5F", "#5FEC5F", "#5F5FEC", "#ECA55F", "#A55FEC"];
+
     if (props.economy == true || props.economy == false) {
       const selectedCoordinates = props.economy
         ? coordinatesSet1
         : coordinatesSet2;
 
       selectedCoordinates.forEach((coord, index) => {
-        addToMap(map, coord);
+        const markerColor =
+          index === 0
+            ? "#FF0000"
+            : index === selectedCoordinates.length - 1
+            ? "#0000FF"
+            : "#1E90FF";
+        addToMap(map, coord, markerColor);
         if (index < selectedCoordinates.length - 1) {
-          addRouteToMap(map, coord, selectedCoordinates[index + 1]);
+          const color = colors[index % colors.length];
+          addRouteToMap(
+            map,
+            coord,
+            selectedCoordinates[index + 1],
+            `route-${index}`,
+            color
+          );
         }
       });
 
@@ -53,11 +68,11 @@ export const Map = (props) => {
     }
   }, [props.economy]);
 
-  const addToMap = (map, coordinates) => {
-    new mapboxgl.Marker().setLngLat(coordinates).addTo(map);
+  const addToMap = (map, coordinates, color) => {
+    new mapboxgl.Marker({ color }).setLngLat(coordinates).addTo(map);
   };
 
-  const addRouteToMap = async (map, start, end) => {
+  const addRouteToMap = async (map, start, end, routeId, color) => {
     const query = await fetch(
       `https://api.mapbox.com/directions/v5/mapbox/driving/${start[0]},${start[1]};${end[0]},${end[1]}?geometries=geojson&access_token=${mapboxgl.accessToken}`
     );
@@ -75,11 +90,11 @@ export const Map = (props) => {
         },
       };
 
-      if (map.getSource("route")) {
-        map.getSource("route").setData(geojson);
+      if (map.getSource(routeId)) {
+        map.getSource(routeId).setData(geojson);
       } else {
         map.addLayer({
-          id: "route",
+          id: routeId,
           type: "line",
           source: {
             type: "geojson",
@@ -90,7 +105,7 @@ export const Map = (props) => {
             "line-cap": "round",
           },
           paint: {
-            "line-color": "#EC5F5F",
+            "line-color": color,
             "line-width": 5,
           },
         });
